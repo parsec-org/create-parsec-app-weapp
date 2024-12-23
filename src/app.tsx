147ed7f581a7523fd54cfab8506c02f4d2ca99a7
hcyang <hqwlkj@outlook.com>
2024-12-23 16:06:15 +0800

    chore: fix eslint file

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,16 @@ import Taro, { useDidShow, useLaunch } from '@tarojs/taro';
 import { ConfigProvider } from '@antmjs/vantui';
 import './app.less';
 
-const updateManager = Taro.getUpdateManager();
-updateManager.onUpdateReady(() => {
-  Taro.showModal({
-    title: '更新提示',
-    content: '新版本已经准备好，即将重启应用。',
-    showCancel: false,
-  }).then(updateManager.applyUpdate);
-});
+if (process.env.TARO_ENV === 'weapp') {
+  const updateManager = Taro.getUpdateManager();
+  updateManager.onUpdateReady(() => {
+    Taro.showModal({
+      title: '更新提示',
+      content: '新版本已经准备好，即将重启应用。',
+      showCancel: false,
+    }).then(updateManager.applyUpdate);
+  });
+}
 
 export default (props: PropsWithChildren) => {
   useLaunch(() => {
@@ -18,7 +20,10 @@ export default (props: PropsWithChildren) => {
   });
 
   useDidShow(() => {
-    console.log(`${process.env.TARO_ENV}运行版本：`, __wxConfig.envVersion);
+    console.log('process.env.TARO_ENV:', process.env.TARO_ENV);
+    if (process.env.TARO_ENV === 'weapp') {
+      console.log(`${process.env.TARO_ENV}运行版本：`, __wxConfig.envVersion);
+    }
   });
 
   // 这里定义你的主题样式
