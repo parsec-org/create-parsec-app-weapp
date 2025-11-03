f60ada69321dd23f3bf000904ed8e65fd009fd6e
hcyang <hqwlkj@outlook.com>
2025-11-03 11:17:31 +0800

    chore(deps): bump tarojs from 4.0.8 to 4.1.7

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [1, 'always', 100],
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'enhance', 'chore', 'test', 'doc', 'refactor', 'style', 'revert'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'enhance', 'chore', 'test', 'doc', 'refactor', 'style', 'revert']],
   },
 };
